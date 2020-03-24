ade8cd428f9f625bfd6bb1a779e47a359909d3c8
CsabaVK <vaganykarc@outlook.com>
2020-03-24 10:56:46 +0100

    Add project files.

---
+++ uzemanyag/Ar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzemanyag
+{
+    class Ar
+    {
+        public Ar(string s)
+        {
+            var t = s.Split(';');
+            Valtozas = DateTime.Parse(t[0]);
+            Benzin = int.Parse(t[1]);
+            Gazolaja = int.Parse(t[2]);
+        }
+
+        public DateTime Valtozas { get; set; }
+        public int Benzin { get; set; }
+        public int Gazolaja { get; set; }
+        public int Diferencial => Math.Abs(Benzin - Gazolaja);
+    }
+
+}
---
+++ uzemanyag/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace uzemanyag
+{
+    
+    class Program
+    {
+        static List<Ar> arak;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F03();
+            F04();
+            Console.ReadKey();
+        }
+
+        private static void F04()
+        {
+            int minv = arak.Min(a => Math.Abs(a.Benzin - a.Gazolaja));
+            Console.WriteLine(minv);
+        }
+
+        private static void F03()
+        {
+            Console.WriteLine($"f3: {arak.Count}");
+        }
+
+        private static void Beolvas()
+        {
+            arak = new List<Ar>();
+            var sr = new StreamReader(@"uzemanyag.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                arak.Add(new Ar(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
